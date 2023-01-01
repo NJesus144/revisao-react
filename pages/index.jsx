f8a5e2475fdb726d467dfc6c85609e6c12e6c6db
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-01 12:22:47 -0300

    Criado estados

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import Input from '../src/components/input/input'
 
 export default function Home() {
   const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleLabelClick = () => {
     console.log('Handle label click')
@@ -14,11 +15,11 @@ export default function Home() {
   const handleSubmitForm = (e) => {
     e.preventDefault()
     console.log('Handle submit form')
+    console.log({ email, password })
   }
 
-  const handleChangeInput = (e) => {
-    setEmail(e.target.value)
-    console.log(email)
+  const handleChangeInput = (event, onChange) => {
+    onChange(event.target.value)
   }
 
   return (
@@ -27,8 +28,18 @@ export default function Home() {
       <label onClick={handleLabelClick}>Voltar a página...</label>
       <form onSubmit={handleSubmitForm}>
         <Title>Titulo da pagina</Title>
-        <Input type="text" placeholder="Digite seu e-mail" onChange={handleChangeInput} />
-        <p>{email}</p>
+        <Input
+          type="text"
+          placeholder="Digite seu e-mail"
+          onChange={(e) => handleChangeInput(e, setEmail)}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={(e) => handleChangeInput(e, setPassword)}
+        />
+        <p>O Email digitado foi: {email}</p>
+        <p>O Senha digitada foi: {password}</p>
         <Button>Botão dentro do form</Button>
       </form>
       <Button onClick={handleLabelClick}>Botão fora do form</Button>

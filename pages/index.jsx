4c3f2768d78c42b9124c7682e635a2a64beb023e
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-01-01 10:06:13 -0300

    Criado exemplo scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,12 @@
 import Title from '../src/components/title/title'
+import Button from '../src/components/button/button'
 
 export default function Home() {
-  return <Title>Titulo da pagina</Title>
+  return (
+    <>
+      <Title>Titulo da pagina</Title>
+      <Button>Button label</Button>
+      <label>LABEL TESTE</label>
+    </>
+  )
 }
